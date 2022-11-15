01b9342269bacbb78d01d965c8ba6c29afeabf0e
yun-ujel <dhall2022@elam.co.uk>
2022-11-15 12:41:10 +0000

    improved movement and added jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float maxSpeed = 0.01f;
+    [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float jumpForce = 300f;
+    [SerializeField, Range(-5f, 5f)] private float groundDrag = 2f;
+    [SerializeField, Range(0f, 10f)] private float airMultiplier;
+
     public Transform orient;
+
     Rigidbody rb;
+
+    Vector3 moveDir;
+
+    public LayerMask ground;
+    bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +24,46 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Vector3 newVelocity = orient.transform.forward * Input.GetAxis("Vertical") * maxSpeed;
-        rb.velocity = new Vector3(newVelocity.x, rb.velocity.y, newVelocity.z);
+        grounded = Physics.Raycast(transform.position, Vector3.down, 1f + 0.2f, ground);
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+
+        if(Input.GetKey(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * (jumpForce * 0.01f), ForceMode.Impulse);
+
+
         
+    }
+
+    private void MovePlayer()
+    {
+        moveDir = (orient.forward * Input.GetAxisRaw("Vertical")) + (orient.right * Input.GetAxisRaw("Horizontal"));
+        if (grounded)
+        {
+            rb.AddForce(moveDir.normalized * moveSpeed, ForceMode.Force);
+        }
+        else if (!grounded)
+        {
+            rb.AddForce(moveDir.normalized * moveSpeed * airMultiplier, ForceMode.Force);
+        }
+       
     }
 }
